e347f0c0c74928e202fa41b3c5711a69b2d35b95
Vũ Hoàng <153354779+hoangvu201@users.noreply.github.com>
2025-04-26 12:50:46 +0700

    Update b9.js

--- b9/b9.js
+++ b9/b9.js
@@ -1,2 +1,3 @@
-let money = +prompt ("Mời bạn nhập vào mọt số")
-alert(money +"VNĐ")
+let money = +prompt ("Mời bạn nhập vào mọt số");
+let result = money.toLocaleString('vi-VN');
+alert(result +" VNĐ");
